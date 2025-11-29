23fe0e9c34c65d2089bb01f4f006fe0302d8231a
Laurent COCAULT <laurent_cocault@yahoo.fr>
2025-11-29 19:45:16 +0100

    cicd: WUI deployment in background

--- wui/src/index.js
+++ wui/src/index.js
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+if (typeof window !== "undefined") {
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("window is not defined. This script is meant to run in a browser environment.");
+}

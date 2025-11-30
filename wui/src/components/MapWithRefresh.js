472fd3c39256f7d269e924bc04221aa21ac4eb4e
Laurent COCAULT <laurent_cocault@yahoo.fr>
2025-11-30 16:30:36 +0100

    cicd: Deploy WUI

--- wui/src/components/MapWithRefresh.js
+++ wui/src/components/MapWithRefresh.js
@@ -34,11 +34,9 @@ export default function MapWithRefresh({ events, position, onRefresh }) {
       >
         <SetMapRef />
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}"
-          attribution="&copy; <a href='https://www.stadiamaps.com/' target='_blank'>Stadia Maps</a> &copy; <a href='https://openmaptiles.org/' target='_blank'>OpenMapTiles</a> &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-          maxZoom={20}
-          minZoom={0}
-          ext="png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+          maxZoom={19}
         />
         {events.map((evt) => (
           <Circle
